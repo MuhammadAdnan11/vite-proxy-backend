9bb11035ab375aeb7d36ca7ea350f60ac161b9aa
MuhammadAdnan11 <adnan191022@gmail.com>
2024-06-02 12:22:09 +0500

    updated

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express= require ('express')
+const app= express()
+const port=4000
+require('dotenv').config()
+app.get('/',(req,res)=>{
+    res.send('hello friends')
+})
+app.get('/twitter',(req,res)=>{
+    console.log("Twitter is here")
+
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`example app listening at port: ${port}`)
+})
